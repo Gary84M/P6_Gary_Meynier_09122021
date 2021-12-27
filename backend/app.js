9c401aaa5646db202b0a5899d9bc70755c788e96
Gary84M <86193312+Gary84M@users.noreply.github.com>
2021-12-27 13:09:58 +0100

    limiter + helmet

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,7 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const path = require("path");
 const cors = require("cors");
+const helmet = require("helmet");
 
 const sauceRoutes = require("./routes/sauce");
 const userRoutes = require("./routes/user");
@@ -17,6 +18,8 @@ mongoose
   .catch(() => console.log("Connexion à MongoDB échouée !"));
 
 const app = express();
+
+app.use(helmet());
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
+const reqLimiter = require("../middleware/limiter");
 
 const userCtrl = require("../controllers/user");
 
 router.post("/signup", userCtrl.signup);
-router.post("/login", userCtrl.login);
+router.post("/login", reqLimiter.limiter, userCtrl.login);
 
 module.exports = router;
